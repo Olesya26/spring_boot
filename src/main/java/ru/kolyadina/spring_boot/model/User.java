8dcc240ccb9271f7e4a11561b264dbd28ea4a6b9
Olesya26 <metisochka.91@mail.ru>
2021-07-27 16:21:11 +0700

    FetchType.EAGER изменен на FetchType.LAZY

--- src/main/java/ru/kolyadina/spring_boot/model/User.java
+++ src/main/java/ru/kolyadina/spring_boot/model/User.java
@@ -44,7 +44,7 @@ public class User implements UserDetails {
     private String email;
 
 
-    @ManyToMany(cascade=CascadeType.MERGE, fetch = FetchType.EAGER)
+    @ManyToMany(cascade=CascadeType.MERGE, fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
